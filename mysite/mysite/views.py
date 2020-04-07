1987db9f3c464b56a47b917a1e9265085c372a56
Sheldon Choi <unyongchoi@gmail.com>
2020-04-07 17:22:30 +0900

    updated work log

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -23,8 +23,8 @@ def cal_extra_work_time(httpRequest) :
     worktimes = """  
         9:10, 8:00, 9:10, 6:50, 7:40,
         10:10, 8:00, 7:40, 8:00, 7:00,
-              8:10, 8:00, 8:00, 8:00,
-        8:00, 8:00, 8:00, 8:00, 8:00
+              8:10, 8:20, 8:00, 8:00,
+        8:20, 8:00, 8:00, 8:00, 8:00
     """
     days, calculated_time, expected_time = parsing_and_calculate_extra_or_overtime(worktimes)
 
